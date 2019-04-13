39585ac8ada4cfff080a8bd7cb1dbcce3c6ea0fe
Miltiades Vasiliades <miltiathis@windowslive.com>
2019-04-13 20:04:32 +0300

    loading

--- src/controller/LoadCommand.java
+++ src/controller/LoadCommand.java
@@ -1,11 +1,23 @@
 package controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+
 public class LoadCommand implements Command {
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("filepath")));
+			doc.setContents(content);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 }

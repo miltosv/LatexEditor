eac1f08fa5b434d1ed3d03102a7fb207df9387b9
Miltiades Vasiliades <miltiathis@windowslive.com>
2019-04-15 01:13:23 +0300

    addLatexCommand

--- src/controller/AddLatexCommand.java
+++ src/controller/AddLatexCommand.java
@@ -1,10 +1,63 @@
 package controller;
 
+import java.util.HashMap;
+
+import model.Document;
 public class AddLatexCommand implements Command {
 
+	HashMap<String,String>latCommands=new HashMap<String,String>();
+	
+	public AddLatexCommand() {
+		latCommands.put("chapter","\\chapter{...} ");
+		latCommands.put("section","\\section{} ");
+		latCommands.put("subsection","\\subsection{} ");
+		latCommands.put("subsubsection","\\subsubsection{} ");
+		latCommands.put("itemize","\\begin{itemize}\r\n" + 
+				"\\item ...\r\n" + 
+				"\\item ...\r\n" + 
+				"\\end{itemize}");
+		
+		latCommands.put("enumeration","\\begin{enumerate}\r\n" + 
+				"\\item ...\r\n" + 
+				"\\item ...\r\n" + 
+				"\\end{enumerate}");
+		
+		latCommands.put("table","\\begin{table}\r\n" + 
+				"\\caption{....}\\label{...}\r\n" + 
+				"\\begin{tabular}{|c|c|c|}\r\n" + 
+				"\\hline\r\n" + 
+				"... &...&...\\\\\r\n" + 
+				"... &...&...\\\\\r\n" + 
+				"... &...&...\\\\\r\n" + 
+				"\\hline\r\n" + 
+				"\\end{tabular}\r\n" + 
+				"\\end{table}");
+		
+		latCommands.put("figure","\\begin{figure}\r\n" + 
+				"\\includegraphics[width=...,height=...]{...}\r\n" + 
+				"\\caption{....}\\label{...}\r\n" + 
+				"\\end{figure}");
+		
+		
+		
+		
+	}
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
+	public void execute(Document doc, String info) {
+		String doctype=doc.getType();
+		String docContents=doc.getContents();
+		String commandToAdd=latCommands.get(info);
+		if(info.equals("chapter") && (doctype.equals("letter") || doctype.equals("article") )) {
+			System.out.println("operation not allowed for this type of document");
+			return;
+		}
+		if (doctype.equals("letter")) {
+			System.out.println("operation not allowed for LETTER");
+			return;
+		}
+		
+		doc.setContents(docContents.concat(commandToAdd));
+				
 
 	}
 
